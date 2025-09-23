63d41f2dfb8c7172d919f8f69e3459b385b0c1e8
Dragons <10523dragons@gmail.com>
2025-09-23 14:40:10 -0700

    turned SixWheel.java into a OpMode instead of a LinearOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl/SixWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl/SixWheel.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.DriverControl;
 import android.annotation.SuppressLint;
 
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.RobotClass;
 import org.firstinspires.ftc.teamcode.RobotUtils;
@@ -13,7 +13,7 @@ import java.util.Objects;
 
 // adds to list of teleop programs as SixWheelDrive
 @TeleOp(name = "SixWheelDrive", group = "driving")
-public class SixWheel extends LinearOpMode {
+public class SixWheel extends OpMode {
 
     RobotClass robot;
     RobotUtils utils;
@@ -22,23 +22,20 @@ public class SixWheel extends LinearOpMode {
     // makes it so I can access the the motors from different children classes
     @SuppressLint("DefaultLocale")
     @Override
-    public void runOpMode() {
+    public void init() { //when you press "INIT"
         // gets hardware mapping from RobotClass.java
         robot = new RobotClass(hardwareMap);
 
         // This Creates A new Utils object. it thinks that the robot is null but that is because it has not been created yet.
         // you must initialize this object otherwise it thinks that the function is static and the function errors  .
         utils = new RobotUtils(robot, telemetry);
-
-        // waits for you to click the start button
-        waitForStart();
-
-        // while the program is running
-        while (opModeIsActive()) {
-            utils.drive(gamepad1.left_stick_x, gamepad1.right_stick_y);
-
-            // check for the MOTIF and display it on the driver hub.
-            MOTIF = utils.CheckForMotif();
-        }
+    }
+    public void start() { //called once at the start when you press play
+        robot.limelight.start();
+    }
+    public void loop() { //loops as long as the program is running
+        utils.drive(gamepad1.left_stick_x, gamepad1.right_stick_y);
+        // check for the MOTIF and display it on the driver hub.
+        MOTIF = utils.CheckForMotif();
     }
 }
