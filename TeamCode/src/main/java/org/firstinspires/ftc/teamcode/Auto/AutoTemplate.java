741ea2adbc733eeba99b98a3e73128bbbd849f60
sp136r <imad0ofus09@gmail.com>
2025-10-27 16:53:16 -0700

    a template for new autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoTemplate.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RobotClass;
+import org.firstinspires.ftc.teamcode.RobotUtils;
+
+@Autonomous(name="LeaveTheLine")
+public class AutoTemplate extends LinearOpMode {
+    RobotClass robot;
+    RobotUtils utils;
+    @Override
+    public void runOpMode() {
+        robot = new RobotClass(hardwareMap);
+        utils = new RobotUtils(robot, telemetry, this);
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            // insert code here
+        }
+    }
+}
